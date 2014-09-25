18cc4b4ff2f9fffff025fd6e3d4aa140033e472d
Troy Rosenberg <troy@roirevolution.com>
2014-09-25 16:23:08 -0400

    Sets up initial styling.

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,8 @@ class Meeting < ActiveRecord::Base
   has_many :attendances
   has_many :users, through: :attendances
 
+  def friendly_date
+    date.strftime('%A %B %e, %Y at %l:%M %P')
+  end
+
 end
